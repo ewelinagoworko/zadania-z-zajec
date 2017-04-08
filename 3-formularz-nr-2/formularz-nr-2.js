7492dc59bf2f6adf1b0bab9164c8b4384e964170
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-04-08 10:50:00 +0200

    commit

--- 3-formularz-nr-2/formularz-nr-2.js
+++ 3-formularz-nr-2/formularz-nr-2.js
@@ -4,13 +4,13 @@
 
 var lastCheckbox = document.getElementById('zgoda2');
 
-console.log(lastCheckbox); // i sprawdzamy, czy na pewno wyloguje nam w konsoli ten ostatni checkbox
+//console.log(lastCheckbox); // i sprawdzamy, czy na pewno wyloguje nam w konsoli ten ostatni checkbox
 
 //robimy zmienna dla wszystkich checkboxow:
 
 var allCheckboxes = document.querySelectorAll('input[type=checkbox]');
 
-console.log(allCheckboxes);
+//console.log(allCheckboxes);
 
 //definiujemy funkcje zwrotna checkboxState, ktora zmienia stan pozostalych checkboxow:
 
@@ -38,32 +38,38 @@ document.getElementById('wyslij').onclick = validateForm;
 
 function validateForm(event) {
 	
-	event.preventDefault(); //zeby sie nie przeladowala strona - chcemy widziec efekty
+	
 	
 	var firstAgreement = document.getElementById('zgoda');
 	var textInputs = document.querySelectorAll('input[type=text]');
 	
-	console.log(textInputs);
 	
 	for (var i=0; i<textInputs.length; i++) {
-		if(textInputs[i].value == '') //jezeli wartosc w input'cie jest pusta
-			console.log('test' + i);
-			
+		if(textInputs[i].value == '') { //jezeli wartosc w input'cie jest pusta, to logujemy tekst, bo to pole jest wymagane do uzupelnienia, zeby moc wyslac forumlarz
+			event.preventDefault();
+			errorMessage(textInputs[i]); //odnosimy sie do funkcji errorMessage				
+	    } else if (textInputs[i].value != '' && textInputs[i].nextElementSibling.tagName == 'SPAN'){ //jezeli moj element nie jest pusty i jest za nim komunikat błędu, to wyrzuć ten komunikat błędu
+		
+			textInputs[i].nextElementSibling.remove();
+		}
 	}
 	
-	if(firstAgreement.checked == false) {
+	if(firstAgreement.checked == false) { //odwolujemy sie do pierwszej zgody, ktora jest wymagana, zeby wyslac formularza
 		event.preventDefault();
 		
-		if (firstAgreement.nextElementSibling.tagName != 'SPAN') {
-			var message = document.createElement('span'); //tworzymy tekst 'to musi byc zaznaczone', ktory sie wyswietli, jezeli ktos nie zaznaczy pierwszego checkboxa i bedzie chcial wyslac pierwszy formularz
-			message.innerText = 'To pole jest wymagane';
-			message.style.color = 'red';
-			firstAgreement.parentNode.insertBefore(message, 	firstAgreement.nextSibling);
-			
-		}
-		
-
-	}
+		 errorMessage(firstAgreement);
 	
+	} else if (firstAgreement.checked == true && firstAgreement.nextElementSibling.tagName == 'SPAN'){
+		firstAgreement.nextElementSibling.remove();
+	}
+}
+
+function errorMessage(element) { //robimy glowna funkcje z tworzeniem napisu blokujacego i odnosimy sie potem do tej funkcji dla elementow typu input=text
+	if (element.nextElementSibling.tagName != 'SPAN') {
+				var message = document.createElement('span'); //tworzymy tekst 'to musi byc zaznaczone', ktory sie wyswietli, jezeli ktos nie zaznaczy pierwszego checkboxa i bedzie chcial wyslac pierwszy formularz
+				message.innerText = 'To pole jest wymagane';
+				message.style.color = 'red';
+				element.parentNode.insertBefore(message, element.nextSibling);
+		} 
 }
 
